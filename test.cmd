0d9f83cb4928e7d7abc391c48db21376f23caef6
Gokhan Kurt <krtgkn@gmail.com>
2021-09-01 23:56:19 +0300

    remove create and website from packages

---
+++ test.cmd
@@ -0,0 +1,7 @@
+SETLOCAL ENABLEEXTENSIONS
+SET me=%~n0
+SET parent=%~dp0
+SET UNITYEXE="S:\Programs\Unity\2021.1.2f1\Editor\Unity.exe"
+SET MODE=editmode
+%UNITYEXE% -batchmode -batchmode -logFile Logs/test.log -projectPath full-sample -runTests -testPlatform editmode -testResults Logs/editmode-results.xml -assemblyNames ReactUnity.Tests;ReactUnity.Editor.Tests
+"Logs/extent.exe" -i Logs/editmode-results.xml -o Logs/editmode-results/
