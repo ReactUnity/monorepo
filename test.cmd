de30c49d12cbea7888035ebec6bbf2f6023d86b4
Gokhan Kurt <krtgkn@gmail.com>
2022-06-14 23:19:07 +0300

    package updates

--- test.cmd
+++ test.cmd
@@ -3,5 +3,5 @@ SET me=%~n0
 SET parent=%~dp0
 SET UNITYEXE="S:\Programs\Unity\2021.1.2f1\Editor\Unity.exe"
 SET MODE=editmode
-%UNITYEXE% -batchmode -batchmode -logFile Logs/test.log -projectPath full-sample -runTests -testPlatform editmode -testResults Logs/editmode-results.xml -assemblyNames ReactUnity.Tests;ReactUnity.Editor.Tests
+%UNITYEXE% -batchmode -batchmode -logFile Logs/test.log -projectPath full-sample -runTests -testPlatform editmode -testResults Logs/editmode-results.xml -assemblyNames ReactUnity.Tests;ReactUnity.Tests.Editor
 "Logs/extent.exe" -i Logs/editmode-results.xml -o Logs/editmode-results/
